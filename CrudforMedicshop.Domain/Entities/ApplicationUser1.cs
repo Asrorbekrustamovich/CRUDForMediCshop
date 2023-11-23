847e5dcaf8acf15374751d31ff9db54c0956b7c8
Asror <asrorrustmaovich007@gmail.com>
2023-11-23 19:11:51 +0500

    asdasdas

--- CrudforMedicshop.Domain/Entities/ApplicationUser1.cs
+++ CrudforMedicshop.Domain/Entities/ApplicationUser1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 namespace CrudforMedicshop.Domain.Entities
 {
     public class ApplicationUser1:IdentityUser
