8a3a69408a209364b9a488404d1ff56c3bfa7835
Asror <asrorrustmaovich007@gmail.com>
2023-12-03 19:13:35 +0500

    awdawd

---
+++ CruDforMedicShopUi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CruDforMedicShopUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
